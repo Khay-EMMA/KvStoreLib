48911275345f444d1c6c906ddaa9ebbd7072c730
Njoku Emmanuel <kalunjoku123@gmail.com>
2021-07-27 13:37:00 +0400

    write key value pair to new file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::{env::Args, iter::Skip};
+
 fn main() {
-    println!("Hello, world!");
+    let mut arguments: Skip<Args> = std::env::args().skip(1);
+    let key: String = arguments.next().unwrap();
+    let value: String = arguments.next().unwrap();
+    println!("The key is '{}' and the value is '{}'", key, value);
+    let content: String = format!("{}\t{}\n", key, value);
+    std::fs::write("kv.db", content).unwrap();
 }
